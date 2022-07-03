81d8d4ef73da41f4f4add97d640a99e8bf768aef
BabyGurlLike <87925971+BabyGurlsILike@users.noreply.github.com>
2022-07-03 10:53:27 -0700

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,185 @@
+<?php
+
+declare(strict_types=1);
+
+namespace babygurllike\AutoSell;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\block\Block;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
+
+class Main extends PluginBase implements Listener {
+
+
+    private $db;
+
+    public function onEnable():void
+    {
+        $this->db = new Config($this->getDataFolder() . "players.yml");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+
+    public function onJoin(PlayerJoinEvent $event):void
+    {
+
+        $player = $event->getPlayer()->getName();
+
+        if(!$this->db->getNested($player)) {
+
+            $this->db->setNested($player, "off");
+            $this->db->save();
+
+        }
+
+    }
+
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $lable, array $args) : bool
+    {
+
+        $prefix = $this->getConfig()->get("prefix");
+
+        switch($cmd->getName())
+        {
+            case "autosell":
+
+                $player = $sender->getName();
+
+                if($sender->hasPermission("autosell.command")){
+
+                    if($sender instanceof Player){
+
+                        if(isset($args[0])) {
+
+                            switch(strtolower($args[0]))
+                            {
+                                case "on":
+
+                                    $this->db->setNested($player, "on");
+                                    $this->db->save();
+                                    $sender->sendMessage($prefix . " " . TextFormat::GREEN . "Toggled AutoSell! (Enabled)");
+                                    return true;
+
+                                case "off":
+
+                                    $this->db->setNested($player, "off");
+                                    $this->db->save();
+                                    $sender->sendMessage($prefix . " " . TextFormat::RED . "Toggled AutoSell! (Disabled)");
+                                    return true;
+                            }
+
+                        } else {
+
+                            $sender->sendMessage($prefix . " " . TextFormat::RED . "Usage: /autosell <on/off>");
+                        }
+
+                    }  else {
+
+                        $sender->sendMessage($prefix . " " . TextFormat::RED . "Tu peux juste use cette command ingame !");
+                    }
+
+                } else {
+
+                    $sender->sendMessage($prefix . " " . TextFormat::RED . "Tu n'a pas la perm de faire cette command !");
+                }
+        }
+
+        return true;
+
+    }
+
+
+    public function onDropPickup(BlockBreakEvent $event):void
+    {
+        $name = $event->getPlayer()->getName();
+        $con = $this->getConfig()->getAll();
+        $item = $event->getBlock()->getId();
+
+        if($event->getPlayer()->hasPermission("autosell.command")) {
+
+            if($this->db->getNested($name) == "on") {
+
+                if(in_array($event->getPlayer()->getWorld()->getFolderName(), $this->getConfig()->get("worlds"))) {
+
+                    if(isset($con[$item])) {
+
+                        $event->setDrops([]);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+    /**
+     * @priority MONITOR
+     *
+     */
+
+
+    public function onBreak(BlockBreakEvent $event):void
+    {
+        $name = $event->getPlayer()->getName();
+        if($event->getPlayer()->hasPermission("autosell.command")){
+            if($this->db->getNested($name) == "on") {
+                if(!$event->isCancelled()) {
+                    if(!$event->getPlayer()->isCreative()) {
+                        if(in_array($event->getPlayer()->getWorld()->getFolderName(), $this->getConfig()->get("worlds"))) {
+
+                            $con = $this->getConfig()->getAll();
+                            $item = $event->getBlock()->getId();
+                            $itemname = $event->getBlock()->getName();
+
+                            if(!(isset($con[$item]))) {
+
+                                $event->getPlayer()->sendTip(TextFormat::RED . "Ce block n'est pas en solde !");
+
+                            } else {
+
+                                $price = (int)$this->getConfig()->get($item);
+                                $ply = $event->getPlayer()->getName();
+                                EconomyAPI::getInstance()->addMoney($ply, (int)$price);
+                                $event->getPlayer()->sendTip(TextFormat::GREEN . "Sold" . TextFormat::AQUA . " " . $itemname ."(s)". TextFormat::GREEN ." for" . TextFormat::YELLOW ." $" . $price);
+
+                            }
+
+                        } else {
+
+                            $event->getPlayer()->sendTip(TextFormat::RED . "Tu ne peux pas use le AutoSell dans ce monde !");
+
+                        }
+
+                    } else {
+
+                        $event->getPlayer()->sendTip(TextFormat::RED."Tu ne peux pas AutoSell en Creatif !");
+
+                    }
+
+                } else {
+
+                    $event->getPlayer()->sendTip(TextFormat::RED . "Tu ne peux pas vendre avec le AutoSell 1");
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
